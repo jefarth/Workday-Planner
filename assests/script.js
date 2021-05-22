80230cbc06137352da786645e3aae85f32ccc4e0
Jeremiah Farthing <jeremiah.farthing@gmail.com>
2021-05-22 17:09:44 -0600

    Init repo

---
+++ assests/script.js
@@ -0,0 +1,34 @@
+// GIVEN I am using a daily planner to create a schedule
+// WHEN I open the planner
+// THEN the current day is displayed at the top of the calendar
+// WHEN I scroll down
+// THEN I am presented with timeblocks for standard business hours
+// WHEN I view the timeblocks for that day
+// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+// WHEN I click into a timeblock
+// THEN I can enter an event
+// WHEN I click the save button for that timeblock
+// THEN the text for that event is saved in local storage
+// WHEN I refresh the page
+// THEN the saved events persist
+
+
+// Create current day display at top of calendar
+// Scroll down to see timeblocks for standard business hours
+// Each timeblock is color coded to indicate if it is in the past, present, or future
+// Can click into a timeblock
+// When clicked user can enter an event into a timeblock
+// Create a save button for that timeblock
+// Save user text for the timeblock in local storage
+// User events saved and present when page refreshed
+
+
+
+// Create timeblock at top of page
+// Create div for timeblock
+
+// Create grid element with 3 columns and 8 rows
+// Create list elements to represent 9-5 workday for each hour
+// Create a form and button element with each timeblock
+// Save the user entered text to the field when the button is pressed
+// Change color of the timeblocks to represent old, current, and future plans
